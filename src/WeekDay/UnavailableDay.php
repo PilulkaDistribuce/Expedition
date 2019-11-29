28410ba8bcd594b589b62d98843b0a1575b05bb2
Jiří Domjen <jiri.domjen@hotmail.com>
2019-11-29 10:04:47 +0100

    add Unavailable interface

--- src/WeekDay/UnavailableDay.php
+++ src/WeekDay/UnavailableDay.php
@@ -3,19 +3,7 @@ declare(strict_types=1);
 
 namespace Pilulka\Expedition\WeekDay;
 
-use DateTimeInterface;
-
-class UnavailableDay
+interface UnavailableDay
 {
-    private $listOfDays;
-
-    public function getAll(): array
-    {
-        return $this->listOfDays;
-    }
-
-    public function setUnavailableDate(DateTimeInterface $date): void
-    {
-        $this->listOfDays = $date->format('d.m.Y');
-    }
+    public function getAll(): array;
 }
